3a3529043c11eaa4ab0a85631d1a3822c6d3c29a
JuanAgustinBereilh <jagustinbereilh@gmail.com>
2021-08-20 22:12:42 -0300

    Mi tercer commit

--- main.c
+++ main.c
@@ -4,10 +4,10 @@
 int main()
 {
     printf("Hello world!\n");
+    printf("Hello  world!\n");
     printf("Hello world!\n");
     printf("Hello world!\n");
     printf("Hello world!\n");
-    printf("Hello world!\n");
-
+int numero=30;
     return 0;
 }
